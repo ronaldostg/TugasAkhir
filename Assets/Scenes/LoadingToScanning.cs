0bb545a2316f5df14cc07d234541061ee97c2594
ronaldostg <ronaldositanggang108@yahoo.com>
2020-03-10 17:36:02 +0700

    membuat halaman peta part 2

---
+++ Assets/Scenes/LoadingToScanning.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DentedPixel;
+
+public class LoadingToScanning : MonoBehaviour
+{
+
+    public GameObject bar;
+    public int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animateBar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void animateBar(){
+        LeanTween.scaleX(bar, 1,time).setOnComplete(loadToScanMarker);
+    }
+
+    void loadToScanMarker(){
+        SceneManager.LoadScene("scan_marker");
+    }
+}
